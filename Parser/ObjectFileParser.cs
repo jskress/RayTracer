e1d78fcad710b3c1b0fafc40c3a3974503bbba32
jskress <jskress@gmail.com>
2023-10-27 08:40:50 -0500

    Chapter 15: Triangles

--- Parser/ObjectFileParser.cs
+++ Parser/ObjectFileParser.cs
@@ -82,21 +82,49 @@ public class ObjectFileParser
     /// <param name="words">The words from the "f" command line.</param>
     private void StoreFace(string[] words)
     {
-        Point[] points = words[1..]
-            .Select(word => Vertices[int.Parse(word) - 1])
+        (Point, Vector)[] points = words[1..]
+            .Select(ParseFaceTerm)
             .Reverse()
             .ToArray();
 
+        _topGroup ??= _currentGroup = new Group();
+
         for (int index = 1; index < points.Length - 1; index++)
         {
-            Triangle triangle = new (points[0], points[index], points[index + 1]);
+            (Point point1, Vector normal1) = points[0];
+            (Point point2, Vector normal2) = points[index];
+            (Point point3, Vector normal3) = points[index + 1];
+
+            Triangle triangle = normal1 == null || normal2 == null || normal3 == null
+                ? new Triangle(point1, point2, point3)
+                : new SmoothTriangle(point1, point2, point3, normal1, normal2, normal3);
 
             Triangles.Add(triangle);
 
-            _topGroup ??= _currentGroup = new Group();
-
             _currentGroup.Add(triangle);
         }
+    }
+
+    /// <summary>
+    /// This method is used to parse a term from a face statement into the referenced
+    /// point and optional normal.
+    /// </summary>
+    /// <param name="spec">The spec to parse.</param>
+    /// <returns>The point and its optional normal vector.</returns>
+    private (Point, Vector) ParseFaceTerm(string spec)
+    {
+        if (spec.Contains('/'))
+        {
+            string[] parts = spec.Split('/', 3);
+
+            if (parts.Length > 2)
+            {
+                return (Vertices[int.Parse(parts[0]) - 1],
+                    Normals[int.Parse(parts[2]) - 1]);
+            }
+        }
+
+        return (Vertices[int.Parse(spec) - 1], null);
     }
 
     /// <summary>
--- Tests/TestObjectFileParser.cs
+++ Tests/TestObjectFileParser.cs
@@ -61,6 +61,19 @@ public class TestObjectFileParser
         vn 0.707 0 -0.707
         vn 1 2 3
         """;
+    private const string SmoothTriangles =
+        """
+        v 0 1 0
+        v -1 0 0
+        v 1 0 0
+        
+        vn -1 0 0
+        vn 1 0 0
+        vn 0 1 0
+
+        f 1//3 2//1 3//2
+        f 1/0/3 2/102/1 3/14/2
+        """;
 
     [TestMethod]
     public void TestGarbageFile()
@@ -180,5 +193,34 @@ public class TestObjectFileParser
         Assert.IsTrue(Directions.In.Matches(parser.Normals[0]));
         Assert.IsTrue(new Vector(0.707, 0, -0.707).Matches(parser.Normals[1]));
         Assert.IsTrue(new Vector(1, 2, 3).Matches(parser.Normals[2]));
+    }
+
+    [TestMethod]
+    public void TestSmoothTriangles()
+    {
+        ObjectFileParser parser = new (content: SmoothTriangles);
+        Group group = parser.Parse();
+
+        Assert.AreEqual(3, parser.Vertices.Count);
+        Assert.AreEqual(3, parser.Normals.Count);
+        Assert.AreEqual(2, parser.Triangles.Count);
+        Assert.AreEqual(2, group.Surfaces.Count);
+        Assert.AreSame(group.Surfaces[0], parser.Triangles[0]);
+        Assert.AreSame(group.Surfaces[1], parser.Triangles[1]);
+        Assert.IsTrue(group.Surfaces[0] is SmoothTriangle);
+        Assert.IsTrue(group.Surfaces[1] is SmoothTriangle);
+
+        SmoothTriangle t1 = (SmoothTriangle) group.Surfaces[0];
+        SmoothTriangle t2 = (SmoothTriangle) group.Surfaces[1];
+
+        Assert.AreSame(parser.Vertices[2], t1.Point1);
+        Assert.AreSame(parser.Vertices[1], t1.Point2);
+        Assert.AreSame(parser.Vertices[0], t1.Point3);
+        Assert.AreSame(parser.Normals[1], t1.Normal1);
+        Assert.AreSame(parser.Normals[0], t1.Normal2);
+        Assert.AreSame(parser.Normals[2], t1.Normal3);
+        Assert.AreSame(t1.Point1, t2.Point1);
+        Assert.AreSame(t1.Point2, t2.Point2);
+        Assert.AreSame(t1.Point3, t2.Point3);
     }
 }
