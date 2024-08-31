e48afbb3c11fbdf0d29f9903d5140f33d542fd13
jskress <jskress@gmail.com>
2024-08-31 18:58:22 -0500

    Added the IOR for water ice.

--- Core/IndicesOfRefraction.cs
+++ Core/IndicesOfRefraction.cs
@@ -16,6 +16,7 @@ public static class IndicesOfRefraction
     public const double Helium = 1.000036;
     public const double Hydrogen = 1.000132;
     public const double Water = 1.333;
+    public const double WaterIce = 1.31;
     public const double Kerosene = 1.39;
     public const double Glass = 1.52;
     public const double Amber = 1.55;
